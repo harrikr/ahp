12d6f25f1ab95f02dd47028bb1b821f826fb49ec
Harri Kurniawan <kurniawanharry165@gmail.com>
2019-05-25 23:28:02 +0700

    first commit
    egen salah datane hrg metu (ng tambah)

--- tambah.php
+++ tambah.php
@@ -1,32 +1,49 @@
 <?php
 	include('config.php');
 	include('fungsi.php');
-
 	// mendapatkan data edit
-	if(isset($_GET['jenis'])) {
-		$jenis	= $_GET['jenis'];
+	if(isset($_GET['id'])) {
+		$id	= $_GET['id'];
 	}
-
+	$query = "SELECT * FROM alternatif ORDER BY id DESC";
 	if (isset($_POST['tambah'])) {
-		$jenis	= $_POST['jenis'];
+		$id	= $_POST['id'];
 		$nama 	= $_POST['nama'];
 
-		tambahData($jenis,$nama);
+		tambahData($id, $nama);
 
-		header('Location: '.$jenis.'.php');
+		header('Location: '.$id.'.php');
 	}
-
 	include('header.php');
+?>
+ <?php
+
+require_once 'config.php';
+
+$query = "SELECT * FROM alternatif ORDER BY id DESC";
+
+$result = mysqli_query($koneksi, $query);
+
 ?>
 
 <section class="content">
-	<h2>Tambah <?php echo $jenis?></h2>
+	<h2>Tambah</h2>
 
 	<form class="ui form" method="post" action="tambah.php">
 		<div class="inline field">
-			<label>Nama <?php echo $jenis ?></label>
-			<input type="text" name="nama" placeholder="<?php echo $jenis?> baru">
-			<input type="hidden" name="jenis" value="<?php echo $jenis?>">
+			<label>Nama </label>
+			
+			<select>
+
+				<?php while($data = mysqli_fetch_assoc($result) > 0){?>
+
+ 					<option value="<?php echo $data['nama']; ?>"><?php echo $data['nama']; ?></option>
+
+				<?php } ?>
+
+			</select>
+
+			<input type="hidden" name="id" value="<?php echo $id?>">
 		</div>
 		<br>
 		<input class="ui green button" type="submit" name="tambah" value="SIMPAN">
@@ -34,3 +51,29 @@
 </section>
 
 <?php include('footer.php'); ?>
+/*
+iki egen salah 
+<section class="content">
+	<h2>Tambah</h2>
+
+	<form class="ui form" method="post" action="tambah.php">
+		<div class="inline field">
+			<label>Nama </label>
+			
+	<select>
+		<option>--pilih alternatif--</option>
+		<?php $sql = mysql_query('SELECT * FROM alternatif order by id ASC;');?>
+			<?php if (mysql_num_rows($sql) > 0) {?>
+				<?php while ($row = mysql_fetch_array($sql)) { ?>
+		<option name="nama" value="<?php echo $row['nama'] ?>"> </option>
+				<?php } ?>
+		<?php } ?>
+	</select>
+
+			<input type="hidden" name="jenis" value="<?php echo $jenis?>">
+		</div>
+		<br>
+		<input class="ui green button" type="submit" name="tambah" value="SIMPAN">
+	</form>
+</section>
+*/
