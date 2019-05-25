a87c408a7e131c88cfced9fd21eaaf64e3d874d4
Barunaindra <barunaindrakesuma17@gmail.com>
2019-05-25 09:42:24 +0700

    Add files via upload
    for harri

---
+++ bobot.php
@@ -0,0 +1,66 @@
+<?php
+	include('config.php');
+	include('fungsi.php');
+
+	$jenis = $_GET['c'];
+
+	include('header.php');
+?>
+<section class="content">
+	
+	<h2 class="ui header">Perbandingan Alternatif &rarr; <?php echo getKriteriaNama($jenis-1) ?></h2>
+	<?php showTabelPerbandingan($jenis,'alternatif'); ?>
+
+
+	<h3 class="ui header">Tabel Tingkat Kepentingan menurut Saaty (1980)</h3>
+			<table class="ui collapsing striped blue table">
+				<thead>
+					<tr>
+						<th>Nilai Numerik</th>
+						<th>Tingkat Kepentingan <em>(Preference)</em></th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td class="center aligned">1</td>
+						<td>Sama pentingnya <em>(Equal Importance)</em></td>
+					</tr>
+					<tr>
+						<td class="center aligned">2</td>
+						<td>Sama hingga sedikit lebih penting</td>
+					</tr>
+					<tr>
+						<td class="center aligned">3</td>
+						<td>Sedikit lebih penting <em>(Slightly more importance)</em></td>
+					</tr>
+					<tr>
+						<td class="center aligned">4</td>
+						<td>Sedikit lebih hingga jelas lebih penting</td>
+					</tr>
+					<tr>
+						<td class="center aligned">5</td>
+						<td>Jelas lebih penting <em>(Materially more importance)</em></td>
+					</tr>
+					<tr>
+						<td class="center aligned">6</td>
+						<td>Jelas hingga sangat jelas lebih penting</td>
+					</tr>
+					<tr>
+						<td class="center aligned">7</td>
+						<td>Sangat jelas lebih penting <em>(Significantly more importance)</em></td>
+					</tr>
+					<tr>
+						<td class="center aligned">8</td>
+						<td>Sangat jelas hingga mutlak lebih penting</td>
+					</tr>
+					<tr>
+						<td class="center aligned">9</td>
+						<td>Mutlak lebih penting <em>(Absolutely more importance)</em></td>
+					</tr>
+				</tbody>
+			</table>
+</section>
+
+
+
+<?php include('footer.php'); ?>
